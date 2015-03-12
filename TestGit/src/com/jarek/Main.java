bf1ed1c596df6f14edd5fe3f14ff890f3670016c
jarek <jarekpc@o2.pl>
2015-03-12 09:49:28 +0100

    dodałem cały projekt z eclipse

---
+++ TestGit/src/com/jarek/Main.java
@@ -0,0 +1,11 @@
+package com.jarek;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("To jest g³ówna klasa ");
+
+	}
+
+}
