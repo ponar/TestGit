dc532d9c7260ed5d0e6c85e73d6675f3c147300b
jarek <jarekpc@o2.pl>
2015-03-12 10:00:51 +0100

    dodałem metode wyświetlającą date:)

--- TestGit/src/com/jarek/Main.java
+++ TestGit/src/com/jarek/Main.java
@@ -1,11 +1,24 @@
 package com.jarek;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("To jest g³ówna klasa ");
-
+		System.out.println("Aktualna data jest dzisiaj:");
+		metoda();
+	}
+	//
+	public static void metoda(){
+		//
+		Date today = new Date();
+		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+		String date = DATE_FORMAT.format(today);
+		System.out.println("Dzisiaj jest: " + date);
+		
 	}
 
 }
