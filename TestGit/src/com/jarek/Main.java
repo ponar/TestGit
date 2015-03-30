a1ad5509a796f1e5ac3d81d94f8a5a40d0118033
jarek <jarekpc@o2.pl>
2015-03-30 09:45:43 +0200

    dodałem metode drukującą wersje javy

--- TestGit/src/com/jarek/Main.java
+++ TestGit/src/com/jarek/Main.java
@@ -10,6 +10,8 @@ public class Main {
 		System.out.println("To jest g³ówna klasa ");
 		System.out.println("Aktualna data jest dzisiaj:");
 		metoda();
+		//
+		my_version();
 	}
 	//
 	public static void metoda(){
@@ -18,7 +20,11 @@ public class Main {
 		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
 		String date = DATE_FORMAT.format(today);
 		System.out.println("Dzisiaj jest: " + date);
-		
+		//
+	}
+	//sprawdzenie wersji javy
+	public static void my_version(){
+		System.out.println(System.getProperty("java.version"));
 	}
 
 }
